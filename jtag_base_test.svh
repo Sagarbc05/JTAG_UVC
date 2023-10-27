d4ce2471a2e3fd86365240d0ffe53da91156e790
Nanda200 <75884706+Nanda200@users.noreply.github.com>
2023-10-27 11:41:23 +0530

    Create jtag_base_test.svh

---
+++ jtag_base_test.svh
@@ -0,0 +1,45 @@
+class jtag_base_test extends uvm_test;
+
+  `uvm_component_utils(jtag_base_test)
+  
+  //---------------------------------------
+  // env instance 
+  //--------------------------------------- 
+  jtag_env env;
+
+  //---------------------------------------
+  // constructor
+  //---------------------------------------
+  function new(string name = "jtag_base_test",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction : new
+
+  //---------------------------------------
+  // build_phase
+  //---------------------------------------
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    // Create the env
+    env = jtag_env::type_id::create("env", this);
+  endfunction : build_phase
+  
+  
+ function void report_phase(uvm_phase phase);
+   uvm_report_server svr;
+   super.report_phase(phase);
+   
+   svr = uvm_report_server::get_server();
+   if(svr.get_severity_count(UVM_FATAL)+svr.get_severity_count(UVM_ERROR)>0) begin
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+     `uvm_info(get_type_name(), "----            TEST FAIL          ----", UVM_NONE)
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+    end
+    else begin
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+     `uvm_info(get_type_name(), "----           TEST PASS           ----", UVM_NONE)
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+    end
+  endfunction 
+
+endclass : jtag_base_test
